89f7dd412ee4f593844c3af4234cb7e8fdc7f25b
RayyanAlFirdausi <rayyanalfirdausy@gmail.com>
2024-03-05 10:40:59 +0700

    Eloquent Orm

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+        // // Coba akses model UserModel
+        // $user = UserModel::all(); // AMbil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+
+        // Tambah data user dengan ELoquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data);   // tambah data ke tabel m_user
+
+
+        // tambahkan data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);  // update data user
+
+        // coba akses model UserModel
+        $user = UserModel::all();   // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
+}
