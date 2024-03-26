4eb905521ad3f83f900306c370620a284195d775
RayyanAlFirdausi <rayyanalfirdausy@gmail.com>
2024-03-26 13:07:32 +0700

    Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,18 +11,28 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager19',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2,
             ],
         );
+        $user->username = 'manager20';
+
         $user->save();
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        $user->wasChanged(['nama', 'username']); // true
+
         return view('user', ['data' => $user]);
     }
 }
+
 
 // $data = [
 //     'level_id' => 2,
